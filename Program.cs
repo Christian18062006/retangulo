3d51d4269bfcbcf10d476d93d3eb109a7bd66fef
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-09 15:45:22 -0300

    criado o esqueleto do projeto junto com o git ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Retângulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retângulo");
+        }
+    }
+}
