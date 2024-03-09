3ee60ac0dce67b87f0e884a2b7437b90fadb8ef2
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-09 16:23:15 -0300

    Adicionada função para calcular área

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ namespace Retângulo
 {
     class Program
     {
+        public static double calcula_area(double altura, double largura)
+        {
+            double area = altura * largura;
+            return (area);
+        }
         static void Main(string[] args)
         {
             double altura;
@@ -13,7 +18,8 @@ namespace Retângulo
             altura = double.Parse(Console.ReadLine());
             Console.WriteLine("digite a largura do retângulo.");
             largura = double.Parse(Console.ReadLine());
-
+            double area=calcula_area(altura, largura);
+            Console.WriteLine(area.ToString("F2"));
         }
     }
 }
